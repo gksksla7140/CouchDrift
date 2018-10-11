47ffe5e05a44721c1c60755f6e2c35c0d2c05861
Justin Han <gksksla7140@gmail.com>
2018-10-10 17:11:38 -0700

    fix app jsx error

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -34,6 +34,3 @@ const App = () => (
 );
 
 export default App;
-
-// <AuthRoute exact path="/login" component={LogInFormContainer} />
-// <AuthRoute exact path="/signup" component={SignUpFormContainer} />
